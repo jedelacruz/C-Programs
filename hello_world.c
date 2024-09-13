15d745fdfb098303411b9186c6902902ba191382
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2024-09-13 16:34:12 +0800

    Create hello_world.c

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+/*
+
+hello_world: Write a program that prints "Hello, World!" to the console.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello, World!");
+
+    return 0;
+}
