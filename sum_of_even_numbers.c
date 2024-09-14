4f0d37e0d738ab90502046f9c264d289de226b00
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2024-09-14 08:32:44 +0800

    Create sum_of_even_numbers.c

---
+++ sum_of_even_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+
+sum_of_even_numbers: Write a program that calculates the sum of all even numbers between 1 and a given integer n (inclusive).
+
+*/
+
+int main() {
+    // Write C code here
+    int num;
+    int eveNum = 0;
+    
+   while(1){
+       printf("Enter a number: ");
+       scanf("%d", &num);
+       
+       if(num == 0){
+           break;
+       }
+       
+       if(num % 2 == 0){
+           eveNum += num;
+       }
+   }
+   
+   printf("%d is the total amount of even number.", eveNum);
+    
+    
+
+    return 0;
+}
